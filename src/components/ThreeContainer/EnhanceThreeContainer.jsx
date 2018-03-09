31305c11b44a906503d0c5ae0309549fac2d9d5d
emile <emile31.roch@gmail.com>
2018-03-09 22:22:28 +0100

    Finish the bee&bombs Cube waves with Daniel Shiffman tutorial

--- src/components/ThreeContainer/EnhanceThreeContainer.jsx
+++ src/components/ThreeContainer/EnhanceThreeContainer.jsx
@@ -13,13 +13,20 @@ const getCubes = scene => {
   }, []);
 };
 
+function getDistance(mesh1, mesh2) {
+  var dx = mesh1.position.x - mesh2.position.x;
+  var dy = mesh1.position.y - mesh2.position.y;
+  var dz = mesh1.position.z - mesh2.position.z;
+  return Math.sqrt(dx * dx + dy * dy + dz * dz);
+}
 const ThreeContainer = () =>
   compose(
     lifecycle({
       componentDidMount() {
         let angle = 0;
         let cubeHeight = 0.1;
-        let cubeNumber = 6;
+        let width = 21;
+        let height = 21;
         // var THREE = THREELib(); // return THREE JS
         // Our Javascript will go here.
         var scene = new THREE.Scene();
@@ -47,17 +54,43 @@ const ThreeContainer = () =>
 
         var controls = new OrbitControls(camera);
 
-        for (let x = 0; x < cubeNumber*2; x += 2) {
-          //first argument is width second is height and third is z axes
-          var geometry = new THREE.BoxBufferGeometry(1, cubeHeight, 1);
-          var material = new THREE.MeshPhongMaterial({
-            color: 0x00ff00,
-            dithering: true
-          });
-          var cube = new THREE.Mesh(geometry, material);
-          cube.position.set(x + 0.2, 0, 0);
-          cube.name = "cube" + x;
-          scene.add(cube);
+        for (let y = 0; y < height * 2; y += 2) {
+          for (let x = 0; x < width * 2; x += 2) {
+            //first argument is width second is height and third is z axes
+            var geometry = new THREE.BoxBufferGeometry(1, cubeHeight, 1);
+            var material;
+            if (y == 20 && x == 20) {
+              material = new THREE.MeshPhongMaterial({
+                color: 0xffc0cb,
+                dithering: true
+              });
+            } else if (y == 0 && x == 40) {
+              material = new THREE.MeshPhongMaterial({
+                color: 0xff0000,
+                dithering: true
+              });
+            } else if (y == 40 && x == 0) {
+              material = new THREE.MeshPhongMaterial({
+                color: 0x00ffff,
+                dithering: true
+              });
+            } else if (y == 0 && x == 0) {
+              material = new THREE.MeshPhongMaterial({
+                color: 0xffffff,
+                dithering: true
+              });
+            } else {
+              material = new THREE.MeshPhongMaterial({
+                color: 0x00ff00,
+                dithering: true
+              });
+            }
+            var cube = new THREE.Mesh(geometry, material);
+
+            cube.position.set(x * 0.7, 0, y * 0.7);
+            cube.name = "cube" + x;
+            scene.add(cube);
+          }
         }
         var ambient = new THREE.AmbientLight(0xffffff, 0.1);
         scene.add(ambient);
@@ -77,14 +110,26 @@ const ThreeContainer = () =>
         spotLight.shadow.camera.fov = 30;
 
         scene.add(spotLight);
-console.log(Math.sin(2))
+
+        console.log(getDistance(getCubes(scene)[0], getCubes(scene)[20]));
+        console.log(getCubes(scene)[20]);
         function animate() {
-          angle += 0.05;
-          cubeHeight = Math.pow(Math.sin(angle), 2) * 100 + 5;
-            getCubes(scene).forEach(cube=>{
-              cube.scale.y = cubeHeight;
-                }
-            )
+          angle += 0.04;
+          let offset = 0;
+          let cubeIndex = 0;
+          getCubes(scene).forEach(cube => {
+            // if (cubeIndex % height === 0) {
+            //   offset = 0;
+            //   cubeIndex = 0;
+            // }
+            offset = getDistance(cube, getCubes(scene)[220]);
+            let angleAndOffset = angle + (offset*0.15);
+            cubeHeight = (Math.pow(Math.sin(angleAndOffset), 2) * -200) - 50;
+            cube.scale.y = cubeHeight;
+            // offset += 0.03;
+            // cubeIndex++;
+          });
+
           requestAnimationFrame(animate);
           renderer.render(scene, camera);
         }
